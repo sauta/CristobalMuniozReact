58690abfb3a8a06a2bdcc493fb7e151cbdad9675
Cristobal <39746703+sauta@users.noreply.github.com>
2023-12-15 01:26:08 -0300

    ahora sirve despues lo hago mejor

--- cristobalmuniozreact/src/components/MatchListComponent.tsx
+++ cristobalmuniozreact/src/components/MatchListComponent.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import MatchedCard from './MatchedCardComponent';
 import { matchedProfiles } from '../data'; // Asume que ya has creado tu archivo de datos
 
-interface MatchListProps {
-    matchedProfiles: Profile[]; // Asumiendo que tienes una interfaz Profile
-}
-
 const MatchList: React.FC = () => {
     return (
         <div>
